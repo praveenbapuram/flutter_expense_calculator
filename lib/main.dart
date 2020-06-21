e155221b9700db591e99ad146947095cd5d5bbc3
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-06-21 23:45:20 +0530

    1) decoration: BoxDecoration for Containor 2) CrossAxisAlignment for Column

--- lib/main.dart
+++ lib/main.dart
@@ -53,20 +53,36 @@ class MyHomePage extends StatelessWidget {
                               child: Row(
                                 children: <Widget>[
                                   Container(
-                                    child: Text(tx.amount.toString()),
-                                    margin: EdgeInsets.symmetric(
-                                      horizontal: 5,
-                                      vertical: 10
+                                    child: Text(
+                                      '\$ ${tx.amount}',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20,
+                                          color: Colors.purple),
                                     ),
-                                    decoration: BoxDecoration(border: Border.all(color:Colors.black,width: 2,)),
+                                    margin: EdgeInsets.symmetric(
+                                        horizontal: 5, vertical: 10),
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                      color: Colors.purple,
+                                      width: 2,
+                                    )),
                                     padding: EdgeInsets.all(10),
                                   ),
                                   Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: <Widget>[
                                       Text(
                                         tx.tittle,
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
                                       ),
-                                      Text(tx.date.toString())
+                                      Text(
+                                        tx.date.toString(),
+                                        style: TextStyle(color: Colors.grey),
+                                      )
                                     ],
                                   )
                                 ],
