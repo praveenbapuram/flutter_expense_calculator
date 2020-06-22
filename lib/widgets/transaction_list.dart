5a4b881dcfea3eb7535d262fa69f8306622d2b0c
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-06-22 23:57:05 +0530

    Converting ListView to ListView.builder(), to increase the performance with lazy loading.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,48 +10,47 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: 300,
-        child: ListView(
-          children: <Widget>[
-            ...transctions
-                .map((tx) => Card(
-                      child: Row(
-                        children: <Widget>[
-                          Container(
-                            child: Text(
-                              '\$ ${tx.amount}',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.purple),
-                            ),
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 5, vertical: 10),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                              color: Colors.purple,
-                              width: 2,
-                            )),
-                            padding: EdgeInsets.all(10),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                tx.tittle,
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                DateFormat('yyyy-MM-dd').format(tx.date),
-                                style: TextStyle(color: Colors.grey),
-                              )
-                            ],
-                          )
-                        ],
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      '\$ ${transctions[index].amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                    margin: EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transctions[index].tittle,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
                       ),
-                    ))
-                .toList()
-          ],
+                      Text(
+                        DateFormat('yyyy-MM-dd')
+                            .format(transctions[index].date),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: transctions.length,
         ));
   }
 }
