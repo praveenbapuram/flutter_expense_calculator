8abcff7586f25b773894705511169e56387cf944
Praveen Kumar <praveenbapuram.explore@gmail.com>
2020-07-13 18:39:03 +0530

    Adding text based on the size of the screen.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -53,13 +53,22 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transctions[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () {
-                      deleteTx(transctions[index].id);
-                    },
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 500
+                      ? FlatButton.icon(
+                          icon: Icon(Icons.delete),
+                          textColor: Theme.of(context).errorColor,
+                          label: Text('Delete'),
+                          onPressed: () {
+                            deleteTx(transctions[index].id);
+                          },
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () {
+                            deleteTx(transctions[index].id);
+                          },
+                        ),
                 ),
               );
             },
